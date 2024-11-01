b390adbbdfaca32c4fa34e6f7fcac03722045d2b
stefanyrogoskiribeiro <108287238+stefanyrogoskiribeiro@users.noreply.github.com>
2024-11-01 12:26:34 -0300

    A

--- Graficos/informacoesGlobais.js
+++ Graficos/informacoesGlobais.js
@@ -1,16 +1,19 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function vizualizarInformacoesGlobais() {
+async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    cosole.log(dados);
-    const paragrafo = document.createElement('p')
-    paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
-    console.log(paragrafo)
-    
-    const container = document.getElementById(‘graficos-container’)
-    container.appendChild(paragrafo)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
   }
+
+  const paragrafo = document.createElement('p')
+  paragrafo.classList.add('graficos-container__texto')
+  paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_pessoas_conectadas}</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${dados.tempo_medio}</span> horas conectadas.`
+
+// código omitido
   
-  vizualizarInformacoesGlobais()
+  const container = document.getElementById(‘graficos-container’)
+  container.appendChild(paragrafo)
+}
+
+vizualizarInformacoesGlobais()
